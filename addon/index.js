9215d0e28b51851678650679883e7861daf2910e
James Allen <james@jamesallen.io>
2015-03-28 02:25:28 -0700

    publish gh-pages branch

--- addon/index.js
+++
@@ -1,3 +0,0 @@
-import EmberPardon from 'ember-pardon/mixins/ember-pardon';
-
-export default EmberPardon;
--- app/mixins/ember-pardon.js
+++
@@ -1,4 +0,0 @@
-import Ember from 'ember';
-import EmberPardon from 'ember-pardon/mixins/ember-pardon';
-
-export default EmberPardon;
